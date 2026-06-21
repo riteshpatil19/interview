6409a46f730737e7115bedbf99a1fea06bf352b0
ritesh patil <riteshpatil79205@gmail.com>
2026-06-21 13:49:07 +0530

    first commit

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "interviewiq-f4d6c.firebaseapp.com",
+  projectId: "interviewiq-f4d6c",
+  storageBucket: "interviewiq-f4d6c.firebasestorage.app",
+  messagingSenderId: "687325917128",
+  appId: "1:687325917128:web:523b1b00c7cdefb23edafd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
