71743bc6570c63c4254b69520c982526fbdb9f12
ritesh patil <riteshpatil79205@gmail.com>
2026-06-22 11:10:27 +0530

    5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:5000"
+export const ServerUrl  = "https://interview-w0ua.onrender.com"
 
 function App() {
 
