059117e31445882ccace2d5d178158e9cb6b511b
ritesh patil <riteshpatil79205@gmail.com>
2026-06-26 08:38:04 +0530

    15

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,18 +13,12 @@ export const googleAuth = async (req,res) => {
             })
         }
         let token = await genToken(user._id)
-        // res.cookie("token" , token , {
-        //     http:true,
-        //     secure:true,
-        //     sameSite:"none",
-        //     maxAge:7 * 24 * 60 * 60 * 1000
-        // }
-        res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-});
+        res.cookie("token" , token , {
+            http:true,
+            secure:true,
+            sameSite:"none",
+            maxAge:7 * 24 * 60 * 60 * 1000
+    });
         
 
         return res.status(200).json(user)
