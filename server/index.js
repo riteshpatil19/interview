f3ca0eec33421aa59358788a07906267f378870a
ritesh patil <riteshpatil79205@gmail.com>
2026-07-06 15:29:20 +0530

    18

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://interview-2-8lp2.onrender.com",
+    origin:"https://interview-client-zdcx.onrender.com",
     credentials:true
 }))
 
